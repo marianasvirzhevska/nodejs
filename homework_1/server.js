1deccedd36800b76c7406f1719713e60c78a00f7
Mariana Svirzhevska <mariana_svirzhevska@epam.com>
2020-03-18 23:31:04 +0200

    Code refactoring

--- homework_1/server.js
+++ homework_1/server.js
@@ -10,13 +10,13 @@ const requestHandler = (request, response) => {
     switch (requestUrl.pathname) {
         case '/logs': {
             const data = fs.readFileSync('./data.json');
-            const { start, end } = requestUrl.query;
+            const { start = 0, end = Infinity } = requestUrl.query;
 
             response.writeHead(200, {'Content-type': 'application/json'});
 
             if (start) {
                 const filtered = JSON.parse(data).logs.filter(log => log.time >= start && log.time <= end);
-                response.write(JSON.stringify(filtered, undefined, '    '));
+                response.write(JSON.stringify(filtered, null, '    '));
                 response.end();
             } else {
                 response.write(data);
@@ -48,12 +48,24 @@ function logRequest({ method, url }) {
         url
     };
 
-    const data = fs.readFileSync('./data.json');
+    let data;
+    try {
+        data = fs.readFileSync('./data.json');
+    } catch (err) {
+        const content = { logs: [] };
+        fs.writeFile('data.json', JSON.stringify(content, null, '    '), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('File created')
+        } );
+    }
+
     const json = parseJson(data);
 
     json.logs.push(log);
 
-    fs.writeFileSync('./data.json', JSON.stringify(json, undefined, '    '))
+    fs.writeFileSync('./data.json', JSON.stringify(json, null, '    '))
 }
 
 function parseJson(data) {
